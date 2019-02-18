628e085a6efc3e993e947f064c9b14614c1eebe4
Michał Wiewiórowski <NSq1>
2019-02-18 19:47:28 +0100

    wip and basic_exercises sections

--- _includes/szkolenie_customize.js
+++ _includes/szkolenie_customize.js
@@ -4,8 +4,15 @@ if (href.indexOf('127.0.0.1') > 0) {
 } else {
     href = href.substr(0, href.lastIndexOf('/szkolenie/')) + '/szkolenie/';
 }
-$.getScript(href + "assets/js/szkolenie.js");
-$.getScript(href + "assets/js/featherlight.min.js");
+var js = { szkolenie : href + "assets/js/szkolenie.js"
+	,	featherlight : href + "assets/js/featherlight.min.js"
+}
+for (var library in js){
+	$.getScript(js[library]);
+	if (console!==undefined){
+		console.log(js[library]);
+	}	
+}
 $('head')
     .append($('<link rel="stylesheet" type="text/css" />')
         .attr('href', href + 'assets/css/szkolenie.css'));
